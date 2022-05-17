52528514b5ffc671813d217a003b6454f495b659
RomanPinelo <prof.roman.pinelo@gmail.com>
2022-05-17 00:46:06 -0500

    Acomodado provisional para subir al link oficial

--- scripts/compra.js
+++ scripts/compra.js
@@ -2,14 +2,14 @@
 var selectEstado = document.getElementById("estado");
 
 // Variables del indicador de páginas
-var paginasView = document.getElementById("paginas");
-var buttonPageLeft = document.getElementById("btnPage_left");
-var buttonPageRight = document.getElementById("btnPage_right");
-var pageXOfY = document.getElementById("paginaxdey");
-var page = 1;
-var margin_Left = 0;
+// var paginasView = document.getElementById("paginas");
+// var buttonPageLeft = document.getElementById("btnPage_left");
+// var buttonPageRight = document.getElementById("btnPage_right");
+// var pageXOfY = document.getElementById("paginaxdey");
+// var page = 1;
+// var margin_Left = 0;
 
-pageXOfY.innerHTML = page;
+// pageXOfY.innerHTML = page;
 
 // Función para cambiar el ancho del select, de acuerdo a la opción seleccionada.
 function anchoSelect() { 
@@ -31,33 +31,33 @@ function anchoSelect() {
   }
 }
 
-// Funciones del indicador de páginas
-buttonPageLeft.addEventListener('click', function(){
-  page -= 1;
-  pageXOfY.innerHTML = page;
-  margin_Left += 100;
-  paginasView.style.marginLeft = `${margin_Left}%`;
-  if (page < 1) {
-    page = 1;
-    pageXOfY.innerHTML = page;
-    margin_Left -= 100;
-    paginasView.style.marginLeft = `${margin_Left}%`;
-  }
-});
-
-buttonPageRight.addEventListener('click', function(){
-  page += 1;
-  pageXOfY.innerHTML = page;
-  margin_Left -= 100;
-  paginasView.style.marginLeft = `${margin_Left}%`;
-  if (page > 2) {
-    page = 2;
-    pageXOfY.innerHTML = page;
-    margin_Left += 100;
-    paginasView.style.marginLeft = `${margin_Left}%`;
-  }
-});
-
 setInterval(() => {
   anchoSelect();
 }, 50);
+
+// Funciones del indicador de páginas
+// buttonPageLeft.addEventListener('click', function(){
+//   page -= 1;
+//   pageXOfY.innerHTML = page;
+//   margin_Left += 100;
+//   paginasView.style.marginLeft = `${margin_Left}%`;
+//   if (page < 1) {
+//     page = 1;
+//     pageXOfY.innerHTML = page;
+//     margin_Left -= 100;
+//     paginasView.style.marginLeft = `${margin_Left}%`;
+//   }
+// });
+
+// buttonPageRight.addEventListener('click', function(){
+//   page += 1;
+//   pageXOfY.innerHTML = page;
+//   margin_Left -= 100;
+//   paginasView.style.marginLeft = `${margin_Left}%`;
+//   if (page > 2) {
+//     page = 2;
+//     pageXOfY.innerHTML = page;
+//     margin_Left += 100;
+//     paginasView.style.marginLeft = `${margin_Left}%`;
+//   }
+// });
