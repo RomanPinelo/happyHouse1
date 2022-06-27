f5dd5a814b236a701f6c5202e3042bef14bacdac
RomanPinelo <prof.roman.pinelo@gmail.com>
2022-06-27 11:40:22 -0500

    Modificación múltiples slider e imágenes

--- scripts/compra.js
+++ scripts/compra.js
@@ -338,6 +338,68 @@ function pintarDatos(datos)  {
     }
     contadorPropiedades++;
   }
+  // Manejador de multiples slider
+  setTimeout(() => {
+    const slideContainer = document.querySelectorAll('.slider');
+
+    for(let i = 0; i < slideContainer.length; i++){
+
+      const slider = function () {
+        // const this_slider = this;
+        const slides = slideContainer[i].querySelectorAll('.slide');
+        const btnLeft = slideContainer[i].querySelector('.slider__btn--left');
+        const btnRight = slideContainer[i].querySelector('.slider__btn--right');
+
+        let curSlide = 0;
+        const maxSlide = slides.length;
+
+        const goToSlide = function (slide) {
+          slides.forEach(
+            (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+          );
+        };
+
+        // Next slide
+        const nextSlide = function () {
+          if (curSlide === maxSlide - 1) {
+            curSlide = 0;
+          } else {
+            curSlide++;
+          }
+
+          goToSlide(curSlide);
+        };
+
+        const prevSlide = function () {
+          if (curSlide === 0) {
+            curSlide = maxSlide - 1;
+          } else {
+            curSlide--;
+          }
+          goToSlide(curSlide);
+        };
+
+        const init = function () {
+          goToSlide(0);
+        };
+        init();
+
+        // Event handlers
+        btnRight.addEventListener('click', nextSlide);
+        btnLeft.addEventListener('click', prevSlide);
+
+        document.addEventListener('keydown', function (e) {
+          if (e.key === 'ArrowLeft') prevSlide();
+          e.key === 'ArrowRight' && nextSlide();
+        });
+
+        setInterval(() => {
+          nextSlide();
+        }, 5000);
+      };
+      slider();
+    }
+  }, 3000);
 }
 
 // Función para mostrar el mensaje Sin Resultados
@@ -638,66 +700,3 @@ botonBusqueda.addEventListener('click', function(){
         filtrarDatos(datos, opcionSelect); //Función para tratar los datos
       })
 });
-
-// Manejador de multiples slider
-setTimeout(() => {
-  const slideContainer = document.querySelectorAll('.slider');
-
-  for(let i = 0; i < slideContainer.length; i++){
-
-    const slider = function () {
-      // const this_slider = this;
-      const slides = slideContainer[i].querySelectorAll('.slide');
-      const btnLeft = slideContainer[i].querySelector('.slider__btn--left');
-      const btnRight = slideContainer[i].querySelector('.slider__btn--right');
-
-      let curSlide = 0;
-      const maxSlide = slides.length;
-
-      const goToSlide = function (slide) {
-        slides.forEach(
-          (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-        );
-      };
-
-      // Next slide
-      const nextSlide = function () {
-        if (curSlide === maxSlide - 1) {
-          curSlide = 0;
-        } else {
-          curSlide++;
-        }
-
-        goToSlide(curSlide);
-      };
-
-      const prevSlide = function () {
-        if (curSlide === 0) {
-          curSlide = maxSlide - 1;
-        } else {
-          curSlide--;
-        }
-        goToSlide(curSlide);
-      };
-
-      const init = function () {
-        goToSlide(0);
-      };
-      init();
-
-      // Event handlers
-      btnRight.addEventListener('click', nextSlide);
-      btnLeft.addEventListener('click', prevSlide);
-
-      document.addEventListener('keydown', function (e) {
-        if (e.key === 'ArrowLeft') prevSlide();
-        e.key === 'ArrowRight' && nextSlide();
-      });
-
-      setInterval(() => {
-        nextSlide();
-      }, 5000);
-    };
-    slider();
-  }
-}, 3000);
